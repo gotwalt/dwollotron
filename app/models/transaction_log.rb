c4600f3ac95bb2687d470d784e6865dcc0dfc121
Aaron Gotwalt <gotwalt@gmail.com>
2012-02-07 12:08:35 -0800

    Cleanup.

--- app/models/transaction_log.rb
+++
@@ -1,6 +0,0 @@
-class TransactionLog < ActiveRecord::Base
-  belongs_to :account
-  
-  validates_presence_of :account_id, :started_at, :completed_at, :remote_transaction_id, :raw_response, :is_successful
-  
-end
