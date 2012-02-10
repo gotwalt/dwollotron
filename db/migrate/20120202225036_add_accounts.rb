bd1479f2c01997e804626ff49cfd4e7e0158755a
Aaron Gotwalt <gotwalt@gmail.com>
2012-02-10 08:51:06 -0800

    Added external_service_id to accounts.

--- db/migrate/20120202225036_add_accounts.rb
+++ db/migrate/20120202225036_add_accounts.rb
@@ -1,11 +1,15 @@
 class AddAccounts < ActiveRecord::Migration
   def self.up
     create_table :accounts do |t|
+      t.integer  :external_service_id, :null => false
       t.string   :access_token, :null => false
       t.string   :pin
       t.integer  :current_payment_id
       t.string   :state
     end
+    
+    add_index :accounts, :external_service_id
+    
   end
 
   def self.down
--- spec/factories/account.rb
+++ spec/factories/account.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :account do
+    sequence(:external_service_id) { |n| n }
     sequence(:access_token) {|n| "oauth-token-#{n}" }
     pin 1234
   end
