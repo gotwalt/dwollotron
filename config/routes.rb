99574df48e0ddea8c2fdefbd17c1b7c62f5081e5
Aaron Gotwalt <gotwalt@gmail.com>
2012-02-10 09:56:47 -0800

    Starting to work on a basic web console for this thing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Dwollotron::Application.routes.draw do
-  resources :accounts
+  resources :accounts, :only => [:index, :show]
+  resources :payments, :only => [:show]
   root :to => 'accounts#index'
 end
