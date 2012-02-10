ce353f1042cbf970d4ba47020573d464d5d15974
Aaron Gotwalt <gotwalt@gmail.com>
2012-02-09 20:03:01 -0800

    AccountQueuer now works, though it's probably not efficient. Consider rewriting when there are more than a few accounts.

--- app/workers/account_queuer.rb
+++ app/workers/account_queuer.rb
@@ -2,7 +2,14 @@ class AccountQueuer
   @queue = :account_queue
   
   def self.perform(args = nil)
-    Account.all.map(&:queue)
+    current_day_of_month = Time.now.getgm.day
+    Account.waiting.map do |account|
+      nil
+      if account.current_withdrawal.day_of_month == current_day_of_month
+        account.queue
+        true
+      end
+    end.compact.count
   end
   
 end
---
+++ spec/workers/account_queuer_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe AccountQueuer do
+  let(:account) { FactoryGirl.create(:account) }
+  before do
+    account.scheduled_withdrawals.create!(:day_of_month => Time.now.getgm.day, :effective_at => Time.now)
+    Account.stub(:waiting).and_return([account])
+  end
+  
+  it 'should find a transaction and start the processing' do
+    account.should_receive(:queue)
+    AccountQueuer.perform
+  end
+  
+end
