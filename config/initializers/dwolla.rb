07ac26b3ef66f48942a11aafcd8c1110f084362b
Aaron Gotwalt <gotwalt@gmail.com>
2012-03-19 10:27:05 -0700

    Cleanup

--- config/initializers/dwolla.rb
+++
@@ -1,7 +0,0 @@
-Dwollotron::Application.configure do
-  c = Struct.new(:key, :secret, :account)
-  config.dwolla = c.new
-  config.dwolla.key = "dsUcI/qPmnWehQsJGyUJNzVJa5SG2dMIA000By4YU8OA+7MIhJ"
-  config.dwolla.secret = "P48HPkf7BcU3ChOi/WNX6Fz4XmNDPc+oAQ4XmvIrPKLzMHfaGW"
-  config.dwolla.account = "812-552-6232"
-end
